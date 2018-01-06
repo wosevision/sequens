9f85f8794060041aa5600ad61d5c347b5a963f2d
Wose <Jacksonteather@hotmail.com>
2018-01-05 22:37:38 -0500

    🛠 Stub in timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
