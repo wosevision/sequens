a8c10a7fa4b01a5be926a73afd0fb3aedfea7f45
Wose <Jacksonteather@hotmail.com>
2018-01-07 00:07:33 -0500

    🛠 WIP stub in layer bits and generated component

--- src/app/timeline/components/timeline.component.ts
+++ src/app/timeline/components/timeline.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
+  layer = {
+    el: document.createElement('div'),
+    get tag() {
+      return (<Element>this.el).localName || 'element';
+    },
+    get class() {
+      const classArray = (<Element>this.el).classList.toString().split(' ');
+      return classArray.length ? `.${classArray.join('.')}` : '';
+    },
+    get id() {
+      const id = (<Element>this.el).id;
+      return id && id !== '' ? `#${id}` : '';
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
+    this.layer.el.setAttribute('id', 'fun_id');
+    this.layer.el.classList.add('fun-class');
+    this.layer.el.classList.add('more-fun');
   }
 
 }
--- src/app/timeline/timeline.module.ts
+++ src/app/timeline/timeline.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { TimelineComponent } from './components/timeline.component';
+import { LayerComponent } from './components/layer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [TimelineComponent],
+  declarations: [TimelineComponent, LayerComponent],
   exports: [TimelineComponent]
 })
 export class TimelineModule { }
