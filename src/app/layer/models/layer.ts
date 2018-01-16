15cd6f58ca424b565e8bc26879917090ab071218
Wose <Jacksonteather@hotmail.com>
2018-01-16 17:59:03 -0500

    🛠 Add more mocking functions to layer model

--- src/app/layer/models/layer.ts
+++ src/app/layer/models/layer.ts
@@ -14,13 +14,31 @@ export interface Layer {
   visible: boolean;
 }
 
-export function generateMockLayer(id): Layer {
+export function getRandomBetween(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function generateMockTween() {
+  const start = getRandomBetween(0, 1);
+  const end = getRandomBetween(start, 1);
+  return {
+    start,
+    end,
+    ease: t => 0.04 * t / (--t) * Math.sin(25 * t)
+  };
+}
+
+export function generateMockLayer(id, tweens: Tween[] = []): Layer {
   return {
     id,
     name: `mock-layer-${id}`,
     el: '#test-el',
-    tweens: [],
     selected: false,
-    visible: true
+    visible: true,
+    tweens
   };
+}
+
+export function generateMockLayers(): Layer[] {
+  return [1, 2, 3, 4, 5, 6, 7, 8].map(i => generateMockLayer(i, [generateMockTween()]));
 }
